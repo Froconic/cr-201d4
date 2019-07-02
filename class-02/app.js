e351c7ebb13f996335c636a3a47c22570e0c3772
Froconic <froconicdesign@gmail.com>
2019-07-02 11:34:04 -0500

    Create app.js

---
+++ class-02/app.js
@@ -0,0 +1,13 @@
+var villains = ["Joker", "Thanos", "Red Skull", "Venom", "Mysterio"];
+
+console.log(villains);
+
+villains.push("Darkseid");
+
+console.log(villains);
+
+console.log(villains.pop());
+
+console.log(villains);
+
+console.table(villains);
